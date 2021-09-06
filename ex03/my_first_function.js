2217bbc4485b011f872c21f2196b0155d5a43641
janae henry <henry_j@qwasar.io>
2021-09-06 03:18:38 +0000

    commit ex00-03

---
+++ ex03/my_first_function.js
@@ -0,0 +1,10 @@
+
+// Following XXXXXX will be a function that will print using console.log("my_first_function")
+function my_first_function() {
+    console.log("my_first_function");   
+}
+
+
+
+my_first_function();
+
